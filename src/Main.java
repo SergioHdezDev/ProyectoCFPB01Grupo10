f06d2cf9b0ca24d0c19e44cb3ef7ff94cc01708c
Sergio Luis Hernández Ensuncho <ingenierosergiohdez@gmail.com>
2025-04-29 21:22:26 -0500

    Se crea metodo para generar el reporte por productos y se crea el .jar

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,9 +17,19 @@ public class Main {
     private static ArrayList<Map<String,Object>> productos = new ArrayList<>();
 
     public static void main(String[] args) {
+        String rutaCarpetaDatos = "datos";
+        File carpeta = new File(rutaCarpetaDatos);
+
+        if(!carpeta.exists()){
+           carpeta.mkdirs();
+        }else{
+            System.out.println("No se pudo crear la carpeta de datos!");
+        }
+
         menu();
 
     }
+
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
         int opcion;
@@ -31,6 +48,7 @@ public class Main {
                 case 2:
                     try{
                         generarReportesVentas();
+                        generarReporteVentasProductos();
                     } catch (IOException e) {
                         System.out.println("Se presentó un error al generar el reporte de ventas");
                     }
@@ -86,13 +104,40 @@ public class Main {
                         new FileOutputStream("datos/reporte_ventas_por_vendedor.csv"), StandardCharsets.UTF_8))) {
 
             for (Map<String,Object> ventas : ventasPorVendedor) {
-                writer.write(ventas.get("nombre_vendedor") + ";" + ventas.get("total_ventas"));
+                writer.write(ventas.get("nombre_vendedor") + ";" + formatearDecimales((Double)ventas.get("total_ventas")));
                 writer.newLine();
             }
         }
 
         System.out.println("Reporte generado: datos/reporte_ventas_por_vendedor.csv");
     }
+
+    public static void generarReporteVentasProductos() throws IOException{
+        ArrayList<Map<String,Object>> ventasProductos = ventasProducto();
+
+        try (BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream("datos/reporte_ventas_por_producto.csv"), StandardCharsets.UTF_8))) {
+            writer.write("CODIGO;NOMBRE;CANTIDAD;PRECIO;TOTAL");
+            writer.newLine();
+
+            ventasProductos.sort(
+                    Comparator.<Map<String, Object>>comparingInt(
+                            m -> Integer.parseInt(m.get("cantidad").toString())
+                    ).reversed()
+            );
+
+            for (Map<String,Object> ventas : ventasProductos) {
+                writer.write(ventas.get("codigo")+";"+ventas.get("nombre")+";"+ventas.get("cantidad")+";"+formatearDecimales((Double)ventas.get("precio"))+";"+formatearDecimales((Double)ventas.get("total")));
+                writer.newLine();
+            }
+        }
+
+        System.out.println("Reporte generado: datos/reporte_ventas_por_producto.csv");
+
+    }
+
+
 
     public static ArrayList<Map<String, Object>> obtenerEmpleados() {
         String archivoEmpleados = "datos/vendedores.csv";
@@ -160,6 +205,59 @@ public class Main {
         return ventas;
     }
 
+    public static ArrayList<Map<String,Object>> ventasProducto() throws IOException {
+        String carpetaArchivos = "datos";
+        ArrayList<Map<String,Object>> ventasTotales = new ArrayList<>();
+        Map<String, Integer> ventasProductos = new HashMap<>();
+
+        try (Stream<Path> paths = Files.walk(Paths.get(carpetaArchivos))){
+            List<Path> archivosCsv = paths
+                    .filter(Files::isRegularFile)
+                    .filter(p->p.getFileName().toString().startsWith("ventas_") && p.getFileName().toString().endsWith(".csv"))
+                    .collect(Collectors.toList());
+
+            for(Path archivo: archivosCsv){
+
+                try (BufferedReader br = new BufferedReader(new FileReader(archivo.toFile()))){
+                    String linea;
+                    boolean primeraLinea = true;
+
+                    while((linea=br.readLine()) != null) {
+                        if(primeraLinea) {
+                            primeraLinea = false;
+                            continue;
+                        }
+                        String[] partes = linea.split(";");
+                        String codigo = partes[0].trim();
+                        int cantidad = Integer.parseInt(partes[1]);
+                        ventasProductos.merge(codigo, cantidad, Integer::sum);
+                    }
+                }
+            }
+
+        }
+
+
+        productos = obtenerProductos();
+
+        for (Map<String, Object>producto : productos){
+            Map<String, Object> v = new HashMap<>();
+            double precio = obtenerPrecio(producto.get("codigo").toString());
+            int cantidad = ventasProductos.getOrDefault(producto.get("codigo"),0);
+
+            v.put("codigo",producto.get("codigo"));
+            v.put("nombre",producto.get("nombre"));
+            v.put("cantidad",cantidad);
+            v.put("precio",precio);
+            v.put("total",cantidad*precio);
+            ventasTotales.add(v);
+
+        }
+        return ventasTotales;
+
+
+    }
+
     public static double obtenerPrecio(String codigoProducto) {
         NumberFormat nf = NumberFormat.getInstance(new Locale("es", "CO"));
 
@@ -175,4 +273,14 @@ public class Main {
         }
         return 0.0;
     }
+
+    public static String formatearDecimales(double valor) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.getDefault());
+        symbols.setDecimalSeparator(',');
+        DecimalFormat df = new DecimalFormat("#.##", symbols);
+
+        String valorFormateado = df.format(valor); // devuelve "2500,75"
+        return valorFormateado;
+    }
+
 }
